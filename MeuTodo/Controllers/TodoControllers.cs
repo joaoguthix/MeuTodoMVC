b21760a02949274cbfd5bfcf202d3500daf042f3
joao vitor <guerreiroxx8@gmail.com>
2022-10-29 05:31:46 -0300

    ConfiguraçãoControllers

---
+++ MeuTodo/Controllers/TodoControllers.cs
@@ -0,0 +1,19 @@
+﻿using MeuTodo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace MeuTodo.Controllers
+{
+    [ApiController]
+    [Route("v1")] //prefixo de rota
+    public class TodoControllers : ControllerBase
+    {
+        [HttpGet]
+        [Route("todos")]
+        public List<Todo> Get()
+        {
+            return new List<Todo>();
+        }
+
+    }
+}
