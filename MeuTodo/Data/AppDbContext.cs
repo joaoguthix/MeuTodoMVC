71818bf2a3721d137f997e237918699323793bbb
joao vitor <guerreiroxx8@gmail.com>
2022-10-29 00:11:43 -0300

    MigrationCriado

---
+++ MeuTodo/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : Microsoft.EntityFrameworkCore.DbContext
+    {
+        public Microsoft.EntityFrameworkCore.DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(
+            DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=shared");
+        
+    }
+}
